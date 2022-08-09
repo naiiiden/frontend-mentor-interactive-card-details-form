e56a47dc8674137712aadc821bfa658e1d650806
naiiiden <na4ko359@gmail.com>
2022-08-09 13:14:23 +0300

    card name add text-transform: uppercase & prevent from typing numbers

--- script/index.js
+++ script/index.js
@@ -1,3 +1,10 @@
+// CARDHOLDER NAME INPUT
 document.querySelector("#cardholder_name").addEventListener("input", () => {
     document.querySelector(".card--name").textContent = document.querySelector("#cardholder_name").value;
 });
+document.querySelector("#cardholder_name").addEventListener("keypress", e => {
+    if (/[0-9\./]+/.test(e.key)) { 
+        e.preventDefault();
+    };
+});
+
